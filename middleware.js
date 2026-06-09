f08bfafefb390188f0f43a22a0ee04bc496d00ab
Abdulaziz Alrayes <abdulazizalrayes@MacBook-Pro.local>
2026-06-09 12:33:49 +0300

    Drop legacy language query parameters

--- middleware.js
+++ middleware.js
@@ -1,6 +1,23 @@
 export const config = { matcher: '/' };
 
 export default function middleware(request) {
+  const url = new URL(request.url);
+  const languageRoutes = {
+    ar: '/ar',
+    de: '/de',
+    it: '/it',
+    es: '/es',
+    fr: '/fr',
+    'zh-Hans': '/zh-Hans',
+    en: '/',
+  };
+  const legacyLanguage = url.searchParams.get('lang');
+  if (Object.prototype.hasOwnProperty.call(languageRoutes, legacyLanguage)) {
+    url.pathname = languageRoutes[legacyLanguage];
+    url.search = '';
+    return Response.redirect(url, 308);
+  }
+
   const accept = request.headers.get('accept') || '';
   if (accept.includes('text/markdown')) {
     return new Response(null, {
